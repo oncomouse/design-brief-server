d3ca994acf114d934b8c9913e004c244d94dc407
oncomouse <oncomouse@gmail.com>
2018-06-06 14:57:45 -0700

    Whoops

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 const adjectives = require('./data/adjectives.json');
 const audiences = require('./data/audience.json');
@@ -7,8 +7,26 @@ const businesses = require('./data/businesses.json');
 const cities = require('./data/cities.json');
 
 const makeRandom = (items) => items[Math.floor(Math.random()*items.length)];
+const randomInt = (low,high) => Math.floor(low + Math.random() * (high - 1));
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  const type = makeRandom(Object.keys(businesses));
+  const name = makeRandom(businesses[type]);
+  const adjs = [];
+  const numAdjectives = randomInt(2,5);
+  for(let i = 0; i < numAdjectives; i++) {
+    adjs.push(makeRandom(adjectives));
+  }
+  res.json({
+    adjectives: adjs,
+    audience: makeRandom(audiences),
+    business: {
+      name,
+      type
+    },
+    city: makeRandom(cities).city
+  })
+})
 app.get('/adjective', (req, res) => res.json(makeRandom(adjectives)));
 app.get('/audience', (req, res) => res.json(makeRandom(audiences)));
 app.get('/business', (req, res) => {
